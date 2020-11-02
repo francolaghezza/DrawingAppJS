41bf7a494876eb2d80364c8b3a0dbe5cb59d9906
Franco David Laghezza <45965253+francolaghezza@users.noreply.github.com>
2020-11-02 12:50:16 +0100

    optimizando código

--- js/paint.js
+++ js/paint.js
@@ -1,13 +1,12 @@
-//Zona de variables globales
+// Zona de variables globales
 var x = 0;
 var y = 0;
 var dibujar = false;
-var circulo = false;
 var borrar = false;
 var borra = false;
 var barra = document.getElementById("barra");
 
-//Canvas
+// Canvas
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const canvas_X = canvas.offsetLeft;
@@ -15,15 +14,17 @@ const canvas_Y = canvas.offsetTop;
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
-//Grosor de los instrumentos de pintura
+// Grosor de los instrumentos de pintura
 var grosor = document.getElementById("grosor");
 
 grosor.addEventListener("change",(e)=>{
+    
     ctx.lineWidth = grosor.value;
 });
 
-//Pintar con el mouse
+// Pintar con el mouse
 canvas.addEventListener("mousedown",(e)=>{
+    
     x = e.clientX - canvas_X;
     y = (e.clientY - canvas_Y) + 19;
     dibujar = true;
@@ -41,13 +42,17 @@ canvas.addEventListener("mousemove",(e)=>{
 
     x = e.clientX-canvas_X;
     y = (e.clientY-canvas_Y) + 19;
-
-    if(dibujar){
-        barra.style.marginLeft = "-100px";
-    }
-
     ctx.lineCap = "round";
 
+    // Pintar
+    if(dibujar){
+        barra.style.marginLeft = "-100px";
+        ctx.lineWidth = grosor.value;
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
+
+    // Borrar
     if (borrar){
         ctx.clearRect(x,y,15,15);
         barra.style.marginLeft = "-100px";
@@ -56,26 +61,10 @@ canvas.addEventListener("mousemove",(e)=>{
             ctx.clearRect(x,y,grosor.value,grosor.value);
         }
     }
-
-    if (dibujar && !circulo){
-        ctx.lineWidth = grosor.value;
-        ctx.lineTo(x,y);
-        ctx.stroke();
-    }
-
-    if (dibujar && circulo){
-        ctx.lineWidth = 3;
-
-        if (grosor.value >1){
-            ctx.lineWidth = grosor.value;
-        }
-
-        ctx.lineTo(x,y);
-        ctx.stroke();
-    }
 });
 
 canvas.addEventListener("mouseup",(e)=>{
+
     x = e.clientX-canvas_X;
     y = e.clientY-canvas_Y;
     ctx.closePath();
@@ -84,13 +73,15 @@ canvas.addEventListener("mouseup",(e)=>{
     barra.style.marginLeft = 0;
 });
 
-//Pintar con eventos de touch (smartphone)
+// Pintar con eventos de touch (smartphone)
 canvas.addEventListener("touchstart", function(e) {
+
     x = e.targetTouches[0].clientX-canvas_X;
     y = e.targetTouches[0].clientY-canvas_Y;
     dibujar = true;
     ctx.beginPath();
     ctx.moveTo(x, y);
+
     if (borra){
         dibujar = false;
         borrar = true;
@@ -99,32 +90,27 @@ canvas.addEventListener("touchstart", function(e) {
 });
 
 canvas.addEventListener("touchmove", function(e) {
+
     e.preventDefault();
     x = e.targetTouches[0].clientX-canvas_X;
     y = e.targetTouches[0].clientY-canvas_Y;
+    ctx.lineCap = "round";
+
+    // Pintar
     if(dibujar){
         barra.style.marginLeft = "-100px";
+        ctx.lineWidth = grosor.value;
+        ctx.lineTo(x,y);
+        ctx.stroke();
     }
-    ctx.lineCap = "round";
+    
+    // Borrar
     if (borrar){
         ctx.clearRect(x,y,15,15);
         barra.style.marginLeft = "-100px";
         if (grosor.value > 1){
             ctx.clearRect(x,y,grosor.value,grosor.value);
         }
-    }
-    if (dibujar && !circulo){
-        ctx.lineWidth = grosor.value;
-        ctx.lineTo(x,y);
-        ctx.stroke();
-    }
-    if (dibujar && circulo){
-        ctx.lineWidth = 5;
-        if (grosor.value > 1){
-            ctx.lineWidth = grosor.value;
-        }
-        ctx.lineTo(x,y);
-        ctx.stroke();
     }
 });
 
@@ -136,39 +122,42 @@ canvas.addEventListener("touchend", function(e) {
     barra.style.marginLeft = 0;
 });
 
-//Modificar color de fondo del canvas
+// Modificar color de fondo del canvas
 var bg_color = document.getElementById("bg_color");
 
 bg_color.addEventListener("change",(e)=>{
+
     canvas.style.backgroundColor = bg_color.value;
     document.body.style.backgroundColor = bg_color.value;
 });
 
-//Modificar color de fondo del canvas
+// Modificar color de fondo del canvas
 var color = document.getElementById("color");
 
 color.addEventListener("change",(e)=>{
+
     ctx.strokeStyle = color.value;
 });
 
 
-//Pincel
+// Pincel
 var pincel = document.getElementById("pincel");
 
 pincel.addEventListener("click",(e)=>{
+    
     borra = false;
     circulo = true;
     
 });
 
-//Borrar
+// Borrar
 var goma = document.getElementById("goma");
 
 goma.addEventListener("click",(e)=>{
    borra = true;
 });
 
-//Borrar todo
+// Borrar todo
  var clear = document.getElementById("clear");
 
  clear.addEventListener("click",(e)=>{
